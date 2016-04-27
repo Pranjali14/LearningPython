cd257a35e9eb407fa571c3c548c429b42898225c
Pranjali <pranjali.roy@gmail.com>
2016-04-27 14:44:50 +0530

    Update make_negative.py

--- make_negative.py
+++ make_negative.py
@@ -3,3 +3,8 @@ def make_negative( number ):
         return number;
     else :
         return -number;
+        
+// using ternary condition
+
+def make_negative( number ):
+    return number if (number <= 0) else -number;
